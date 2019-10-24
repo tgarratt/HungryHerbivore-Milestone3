cb2a4d458b63bac6e7d717442e765b4cfeb27730
Ubuntu <ubuntu@ip-172-31-86-187.ec2.internal>
2019-10-24 10:54:04 +0000

    Linked mongo to cloud 9 using enviroment variable for database protection

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template(), redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'hungryHerbivore'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URL')
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'testing'
+@app.route('get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
 if __name__ ==  '__main__':
     app.run(host=os.environ.get('IP'),
