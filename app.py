7075cf109dbd62814f00dbcede5a1cc23f5493c8
Ubuntu <ubuntu@ip-172-31-86-187.ec2.internal>
2019-10-30 12:12:58 +0000

    Template for adding recipe inserted

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ mongo = PyMongo(app)
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html')
+    
 if __name__ ==  '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
