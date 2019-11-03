a2015d25430333758f7691e6626fc7cb80a7c605
Ubuntu <ubuntu@ip-172-31-86-187.ec2.internal>
2019-11-03 18:08:59 +0000

    added recipie modification buttons, page and app route

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ def submit_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
     
+@app.route('/modify_recipe/<recipe_id>')
+def modify_recipe(recipe_id):
+    the_recipe = mongo.db.tasks.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('modifyrecipe.html', recipe=the_recipe)
+    
     
 if __name__ ==  '__main__':
     app.run(host=os.environ.get('IP'),
