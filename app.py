98d121ac9bf382fe8ce03700f5da0a8ae0488128
Ubuntu <ubuntu@ip-172-31-86-187.ec2.internal>
2019-11-21 15:33:40 +0000

    further fiddle with the @route filter

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ def view_recipe(recipe_id):
   
 @app.route('/filter', methods=["POST"])
 def filter():
-    difficulty = request.form['difficulty-filter']
-    result_cursor = mongo.db.recipes.find({},{"difficulty": difficulty})
+    difficulty : request.form['difficulty-filter']
+    result_cursor = mongo.db.recipes.find({"difficulty" : difficulty})
     my_res = ""
     for doc in result_cursor:
         my_res += str(doc)
