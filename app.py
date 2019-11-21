07f47f33b3487985413d8bda740324edd4c2db30
Ubuntu <ubuntu@ip-172-31-86-187.ec2.internal>
2019-11-21 12:33:59 +0000

    removed "method post" from app route

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def view_recipe(recipe_id):
   all_categories = mongo.db.categories.find()
   return render_template('viewrecipe.html', recipe=the_recipe)
   
-@app.route('/filter', methods=['POST'])
+@app.route('/filter')
 def filter():
     difficulty =request.form['difficulty-filter']
     db.collection.find({},{"difficulty": difficulty})
